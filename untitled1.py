d695595a367e132b314fd42d2f7aa05c2322a3af
IceSuger <icesugernowater@gmail.com>
2016-10-14 22:36:57 +0800

    added 2016 data
    2016-10-13 以前的数据被加上了，一起来训练的模型

--- untitled1.py
+++ untitled1.py
@@ -46,10 +46,14 @@ def makeTable():
         chunks.append(history)
     return pd.concat(chunks, ignore_index=True)
 
-def fillWithHuangli(history):
+def splitNums(history):
     history[['1','2','3','4','5','6']] = pd.DataFrame([ x.split(',') for x in history['nums'].tolist() ])
     history[['6','7']] = pd.DataFrame([ x.split('|') for x in history['6'].tolist() ])
     history.drop('nums', axis=1, inplace=True)
+    return history
+
+def fillWithHuangli(history):
+    print history
     history[['y','m','d']] = pd.DataFrame([ getHuangli(x) for x in history['date'].tolist() ])
     print 'ok'
     return history
@@ -65,15 +69,18 @@ def prepare(history):
     
 def get2016():
     soup = BeautifulSoup(urllib.urlopen("20162016.html")) 
-    target2016 = []
+    target2016 = [[],[],[],[],[],[],[],[]]
     date2016 = []
     list_nums = soup.find_all('tr',{'class' : 't_tr1'})
     for x in list_nums:
         #位置1~7正好是7个球的号码，位置15为开奖日期
-        target2016.append( x.find_all('td')[7].text )
+        for i in range(1,8):
+            target2016[i].append( x.find_all('td')[i].text )
+        #target2016.append( x.find_all('td')[7].text )
         date2016.append( x.find_all('td')[15].text )
-    target2016 = pd.DataFrame(target2016)
-    target2016.columns=['7']
+    target2016 = pd.DataFrame(target2016).T.drop([0], axis=1)
+    print target2016
+    target2016.columns=['1','2','3','4','5','6','7']
     date2016 = pd.DataFrame(date2016)
     date2016.columns=['date']
     
@@ -84,6 +91,8 @@ def get2016():
     
     return df2016
 
+
+
 #history = fillWithHuangli( makeTable() )
 #dummed = prepare(history) 
 #dummed.to_csv("2003~2015.txt", encoding='gb2312', index=False)
@@ -92,26 +101,30 @@ def get2016():
 df = pd.read_csv("2003~2015.txt",encoding='gb2312') #03~15年的所有中奖号码和五行数据（包括原始的和吖编码之后的）
 history = pd.read_csv("history.txt",header=None) #03~15年的所有中奖号码 第0列为日期
 print "history:"
-print history
+#print history
 patt = df.head(1)
 print 'patt:'
-print patt
+#print patt
 #df2016 = get2016()
 #df2016.to_csv("2016.txt",encoding='gb2312', index=False)
 df2016 = pd.read_csv("2016.txt",encoding='gb2312')
-print df2016
+#print df2016
 
 #f2016 = pd.concat([patt,df2016], axis=0) #按正确格式排过的df2016
-f2016 = patt.merge(df2016,how='outer')
+f2016 = patt.merge(df2016,how='outer').drop(0).fillna(0)
 print 'f2016:\n'
-print f2016
+#print f2016
 
 targets = [[],[],[],[],[],[],[],[]]
-for x in range(1,8):
-    targets[x] = history[x]
+
 features = df.drop(['date','1','2','3','4','5','6','7','y','m','d'], axis=1)
-features2016 = f2016.fillna(0).drop(['date','1','2','3','4','5','6','7','y','m','d',u'y_山下火'], axis=1)
-target2016 = f2016['7'].fillna(0)
+features[u'y_山下火'] = 0.0
+#features2016 = f2016.fillna(0).drop(['date','1','2','3','4','5','6','7','y','m','d',u'y_山下火'], axis=1)
+features2016 = f2016.fillna(0).drop(['date','1','2','3','4','5','6','7','y','m','d'], axis=1)
+target2016 = f2016['7']
+
+for x in range(1,8):
+    targets[x] = pd.concat( [ history[x], f2016[str(x)] ], ignore_index = True)
 #features = df
 #lr = LogisticRegression()
 
@@ -132,7 +145,7 @@ for x in range(1,8):
     #lrs.append(lr.fit(features, targets[x]))
     print fs[x].score(features, targets[x])
 """
-
+"""
 #f2016.to_csv("f2016.txt", index=False, encoding='gb2312')
 forest = RandomForestClassifier(max_depth = 10, min_samples_split=2, n_estimators = 1000, random_state = 1)
 forest.fit(features, history[7])
@@ -140,9 +153,87 @@ print "RF precision old:"
 print forest.score(features, history[7])
 print "RF precision 2016:"
 print forest.score(features2016, target2016)
+"""
+"""
+#下面加上2016年数据一块训练，并预测最后一期
+forest = RandomForestClassifier(max_depth = 10, min_samples_split=2, n_estimators = 1000, random_state = 1)
+newfeat = pd.concat([features,features2016.drop(range(119,121)) ])
+newtar = pd.concat( [ history[7], target2016.drop(range(119,121)) ])
+new_test_feat = features2016[119:121]
+new_test_tar = target2016[119:121]
+forest.fit(newfeat,newtar)
+print "predict with 2016 data:"
+print forest.score(new_test_feat, new_test_tar)
+print forest.predict(new_test_feat)
+print forest.predict_proba(new_test_feat)
+"""
 
 
 
+
+
+
+
+#下面加上2016年数据一块训练，并预测要买的号码
+fs = []
+fs.append('')
+newfeat = pd.concat([features,features2016 ],ignore_index = True)
+"""
+print newfeat.shape
+for i in range(1,8):
+    #print targets[i].shape
+    
+    fs.append( RandomForestClassifier(max_depth = 10, min_samples_split=2, n_estimators = 1000, random_state = 1))
+    fs[i].fit(newfeat,targets[i])
+
+
+
+#预测号码
+a = [{'date':'2016-10-16'}]
+pred_df = pd.DataFrame(a)
+print pred_df
+pred_df = prepare(fillWithHuangli(pred_df))
+pred_df = patt.merge(pred_df,how='outer').drop(0).fillna(0)
+print pred_df
+
+predict_feat = pred_df.fillna(0).drop(['date','1','2','3','4','5','6','7','y','m','d'], axis=1)
+
+for i in range(1,8):
+    print fs[i].predict(predict_feat)
+    
+    
+"""
+    
+    
+    
+   
+#用最后十组 测试准确率
+ntargets = [[],[],[],[],[],[],[],[]]
+print 'shape: ',newfeat.shape
+newf = newfeat.drop(range(2008,2019))
+for i in range(1,8):
+    ntargets[i] = targets[i].drop(range(2008,2019))
+    fs.append( RandomForestClassifier(max_depth = 10, min_samples_split=2, n_estimators = 1000, random_state = 1))
+    fs[i].fit(newf,ntargets[i])
+
+new_test_feat = newfeat[2008:2019]
+print 'fea:'
+print new_test_feat
+
+for i in range(1,8):
+    print fs[i].score(new_test_feat, targets[i][2008:2019])
+
+
+"""
+newtar[7] = pd.concat( [ history[7], target2016 ])
+new_test_feat = features2016[119:121]
+new_test_tar = target2016[119:121]
+forest.fit(newfeat,newtar)
+print "predict with 2016 data:"
+print forest.score(new_test_feat, new_test_tar)
+print forest.predict(new_test_feat)
+print forest.predict_proba(new_test_feat)
+"""
 #df2016 = get2016()
 #print df2016
 #test_features = df2016.drop(['y','m','d'], axis=1).values
